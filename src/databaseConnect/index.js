6c645f35ed67d74bb00feba69ec4dc6c6f74c1e7
HHERVII <heiver.vargas@gmail.com>
2022-11-07 09:47:34 -0500

    pool

--- src/databaseConnect/index.js
+++ src/databaseConnect/index.js
@@ -1,25 +1,27 @@
 /* eslint-disable no-console */
 /* eslint-disable no-else-return */
+const { log } = require("console");
 const mysql = require("mysql");
 
 const data = {
     user: "root",
     password: "123456!Bases",
-    host: "localhost",
+    host: "24.199.68.208",
     database: "pruebas",
-    connectTimeout: 30000,
-    port: "3307",
+    connectionLimit: 10,
+    port: "3306",
+    multipleStatements: true,
 };
 let connection;
 try {
-    connection = mysql.createConnection(data);
-    connection.connect((err) => {
+    connection = mysql.createPool(data);
+    connection.getConnection(function (err, conn) {
         if (err) {
-            console.log("error");
+            console.log("mala conexion");
             console.log(err);
-        } else {
-            console.log("exito en la conexion");
+            return;
         }
+        console.log("exitoo");
     });
 } catch (e) {
     console.log(e);
